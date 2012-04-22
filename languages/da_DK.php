1eeefc731fe4c0e70776f9cb4161af121224cccc
David Andersen <mrdavidandersen@gmail.com>
2012-04-22 20:09:50 +0000

    Added Danish translations

---
+++ languages/da_DK.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * translation data for english
+ * see constant LANGUAGE for current langauage.
+ *
+ * copyright @ Open-org.com, alrights reserved
+ */
+
+$language = array(
+  'Register' => '',
+  'Login' => '',
+  'Help' => 'Hj&aelig;lp',
+  'logo text' => 'F&aring;Svar.dk',
+  'slogan text' => 'Sp&oslash;rg en advokat',
+  'slogan2 text' => 'Stil et sp&oslash;rgsm&aring;l til en advokat. F&aring; et svar s&aring; hurtig som muligt!',
+  'slogan3 text' => 'Vi har advokater indenfor de fleste specialiteter',
+  'slogan4 text' => '',
+  'How it works' => '',
+  'About' => 'Om F&aring;Svar.dk',
+  'Become an expert' => '',
+  'Submit' => 'F&Aring; SVAR!',
+);
