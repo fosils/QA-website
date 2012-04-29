683e2e78051beb97a445d3bc45de66af6a04bc12
fosils <mrdavidandersen@gmail.com>
2012-04-30 02:08:46 +0300

    Added Google Analytics code

--- templates/question_2.php
+++ templates/question_2.php
@@ -5,6 +5,19 @@
 <title><${{title}}></title>
 <link href="styles.css" rel="stylesheet" type="text/css" />
 </head>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-31289213-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <body>
 <div id="container">
   <div id="topmost">
