7025aa1ade56b7819613c9dfde9352e9c1da407b
fosils <mrdavidandersen@gmail.com>
2012-04-30 15:11:51 +0300

    Modified the look, moving the terms text below the form.

--- templates/question_1.php
+++ templates/question_1.php
@@ -95,7 +95,6 @@
       <div class="contentwrapper">
         <div class="contentwrapper">
 	  <div id="error_msg"></div>
-	  <div class="information">{{$60 question}}</div>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" id="paypalform">
 <!--          <form action="index.php?q=question" method="post"> -->
 	    <input type="hidden" name="<C{{STEP_FORM_FIELD}}>" id="<C{{STEP_FORM_FIELD}}>" value="2" />
@@ -124,10 +123,12 @@
 		</select>
 	      </label>
 	    </div>
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" id="hosted_button_id" value="">
-<input id="paypalbutton" class="paypal" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" id="hosted_button_id" value="">
+		</br>
+		<div class="information">{{$60 question}}</div>
+		<input id="paypalbutton" class="paypal" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+		<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
           </form>
         </div>
       </div>
